b486b3d75b065a3bc05953ee2c30e4835a247a56
tronross <john_ross@analogforever.ca>
2023-04-30 21:06:58 -0400

    Fix profileGenerator (refactor as Promise)

--- survey.js
+++ survey.js
@@ -6,19 +6,48 @@ const rl = readline.createInterface({
 });
 
 
-const profileBuilder = function() {
-  let answers = [];
-const name = rl.question("What's your name?", (answer) => {
-  rl.close();
-  answers.push(answer);
- 
-})
-return answers;
+const askMe = q => new Promise (resolve =>
+  rl.question(q, response => resolve(response))
+  );
+
+const profileGen = async () => {
+  const name = await askMe ("What's your handle? ");
+  const activity = await askMe ("What is something you enjoy doing? ");
+  const listen = await askMe ("What do you like listening to? ");
+  const meal = await askMe ("What is your favorite meal? ");
+  const food = await askMe ("What's your favourite thing to eat for that meal? ");
+  const sport = await askMe ("What's your favorite sport? ");
+  const superpower = await askMe ("What is your superpower?");
+  console.log(`\nI'm ${name}! I love ${activity} while listening to ${listen}.`)
+  console.log(`I really enjoy eating ${food} for ${meal} while watching ${sport}.`)
+  console.log(`My SUPERPOWER?! If you really need to know, it's: ${superpower}. Don't tell anyone.`)
+  rl.close()
 }
 
+profileGen();
 
 
-console.log(profileBuilder());
+// rl.question("What's your handle?"), (name => {
+//   rl.question("What is something you enjoy doing?"), (activity => {
+//     rl.question("What do you like listening to?"), (listen => {
+//       rl.question("What is your favorite meal (eg: dinner, breakfast)?"), (meal => {
+//         rl.question("What's your favourite thing to eat for that meal?"), (food => {
+//           rl.question("Which sport is your absolute favourite?"), (sport => {
+//             rl.question("What is your superpower?"), (superpower => {
+//               console.log(`\nI'm ${name}! I love ${activity} while listening to ${listen}.`)
+//               console.log(`I really enjoy eating ${food} for ${meal} while watching ${sport}.`)
+//               console.log(`My SUPERPOWER?! If you really need to know, it's: ${superpower}. Don't tell anyone.`)
+//               rl.close()
+//             });
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+
+
+
 
 
 
